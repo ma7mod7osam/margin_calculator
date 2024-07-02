27c41529580acc393cc10a5f344288c15738d2c3
ma7mod7osam <ma7mod7osam@gmail.com>
2024-07-03 01:54:50 +0300

    Update hooks.py

--- margin_calculator/hooks.py
+++ margin_calculator/hooks.py
@@ -1,3 +1,12 @@
+app_name = "margin_calculator"
+app_title = "Margin Calculator"
+app_publisher = "Mahmood"
+app_description = "App to calculate and apply margins on quotations, sales orders, and sales invoices"
+app_icon = "octicon octicon-file-directory"
+app_color = "grey"
+app_email = "ma7mod7osam@gmail.com"
+app_license = "MIT"
+
 doc_events = {
     "Quotation": {
         "before_insert": 
