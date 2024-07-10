7fe68fb970844049f8593976bf09b52c2c19a1c5
ma7mod7osam <ma7mod7osam@gmail.com>
2024-07-11 01:52:07 +0300

    Update utils.py

--- margin_calculator/utils.py
+++ margin_calculator/utils.py
@@ -5,10 +5,8 @@ def calculate_margin(doc, method):
         # Get the customer document
         customer = frappe.get_doc("Customer", doc.customer)
         
-        # Check if the custom_client_margin_rate field exists and has a 
-value
-        if hasattr(customer, "custom_client_margin_rate") and 
-customer.custom_client_margin_rate:
+        # Check if the custom_client_margin_rate field exists and has a value
+        if hasattr(customer, "custom_client_margin_rate") and customer.custom_client_margin_rate:
             # Retrieve the item valuation rate in the warehouse
             valuation_rate = frappe.db.get_value("Stock Ledger Entry", {
                 "item_code": item.item_code,
@@ -20,4 +18,3 @@ customer.custom_client_margin_rate:
                 margin_rate = valuation_rate * (1 + customer.custom_client_margin_rate / 100.0)
                 item.rate = margin_rate
                 item.amount = item.qty * item.rate
-
